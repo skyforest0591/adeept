03a299f922333bfec326b4a66835acc002dce776
simon <simon@jiazhufuzhou.com>
2020-01-18 23:54:27 -0500

    1/19

--- server/findline.py
+++ server/findline.py
@@ -8,10 +8,11 @@ import RPi.GPIO as GPIO
 import time
 import move
 import servo
+import LED
 
-line_pin_right = 20
+line_pin_right = 19
 line_pin_middle = 16
-line_pin_left = 19
+line_pin_left = 20
 
 def setup():
     GPIO.setwarnings(False)
@@ -21,52 +22,85 @@ def setup():
     GPIO.setup(line_pin_left,GPIO.IN)
     #motor.setup()
 
+led = LED.LED()
 turn_status = 0
+speed = 75
+angle_rate = 1
+color_select = 1 # 0 --> white line / 1 --> black line
+check_true_out = 0
+backing = 0
+last_turn = 0
 def run():
-    global turn_status
+    global turn_status, speed, angle_rate, color_select, led, check_true_out, backing, last_turn
     status_right = GPIO.input(line_pin_right)
     status_middle = GPIO.input(line_pin_middle)
     status_left = GPIO.input(line_pin_left)
     #print('R%d   M%d   L%d'%(status_right,status_middle,status_left))
-    if status_middle == 1:
-        print('middle')
-        turn_status = 0
-        move.move(90, 'forward')
-    elif status_left == 1:
+    
+    if status_right == color_select:
+        check_true_out = 0
+        backing = 0
         print('left')
+        led.colorWipe(0,255,0)
         turn_status = -1
-        servo.turnLeft()
-        move.move(90, 'forward')
-    elif status_right == 1:
-        turn_status = 1
+        last_turn = -1
+        servo.turnLeft(angle_rate)
+        move.move(speed, 'forward')
+    elif status_left == color_select:
+        check_true_out = 0
+        backing = 0
         print('right')
-        servo.turnRight()
-        move.move(90, 'forward')
+        turn_status = 1
+        last_turn = 1
+        led.colorWipe(0,0,255)
+        servo.turnRight(angle_rate)
+        move.move(speed, 'forward')
+
+    elif status_middle == color_select:
+        check_true_out = 0
+        backing = 0
+        print('middle')
+        led.colorWipe(255,255,255)
+        turn_status = 0
+        servo.turnMiddle()
+        move.move(speed, 'forward')
+        # time.sleep(0.2)
+    
     else:
         print('none')
-        if not turn_status  == 0:
-          if turn_status == -1:
-            servo.turnLeft()
-          else:
-            servo.turnRight()
-        move.move(90, 'forward')
-        
-        '''
-        if not turn_status  == 0:
-          if turn_status == 1:
-            servo.turnLeft()
-          else:
-            servo.turnRight()
-        move.move(90, 'backword')
-        '''
+        led.colorWipe(255,0,0)
+        if not backing == 1:
+            if check_true_out == 1:
+                check_true_out = 0
+                if turn_status == 0:
+                    if last_turn == 1:
+                        servo.turnRight(angle_rate)
+                    else:
+                        servo.turnLeft(angle_rate)
+                    move.move(speed, 'backward')
+                    time.sleep(0.3)
+                elif turn_status == 1:
+                    time.sleep(0.3)
+                    servo.turnLeft(angle_rate)
+                else:
+                    time.sleep(0.3)
+                    servo.turnRight(angle_rate)
+                move.move(speed, 'backward')
+                backing = 1
+                # time.sleep(0.2)
+            else:
+                time.sleep(0.1)
+                check_true_out = 1
 
 if __name__ == '__main__':
     try:
         setup()
         move.setup()
+        
         while 1:
             run()
             
         pass
     except KeyboardInterrupt:
         move.destroy()
+
